f25545bfe0a7bf90f4ed0fa41e4f30f548e5ccbb
Ulysse Gérard <thevoodoos@gmail.com>
2021-06-07 21:07:41 +0200

    Attempt to style mobile menu

--- themes/afj/js/site.js
+++ themes/afj/js/site.js
@@ -3,7 +3,7 @@ var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
 function scrollHeader() {
     // Has scrolled class on header
     var zvalue = $(document).scrollTop();
-    if ( zvalue > 75 )
+    if (zvalue > 75)
         $("#header").addClass("scrolled");
     else
         $("#header").removeClass("scrolled");
@@ -13,13 +13,13 @@ function parallaxBackground() {
     $('.parallax').css('background-positionY', ($(window).scrollTop() * 0.3) + 'px');
 }
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
     scrollHeader();
 
     // Scroll Events
-    if (!isTouch){
-        $(document).scroll(function() {
+    if (!isTouch) {
+        $(document).scroll(function () {
             scrollHeader();
             parallaxBackground();
         });
@@ -27,13 +27,13 @@ jQuery(document).ready(function($){
 
     // Touch scroll
     $(document).on({
-        'touchmove': function(e) {
+        'touchmove': function (e) {
             scrollHeader(); // Replace this with your code.
         }
     });
 
     //Smooth scroll to start
-    $('#to-start').click(function(){
+    $('#to-start').click(function () {
         var start_y = $('#start').position().top;
         var header_offset = 45;
         window.scroll({ top: start_y - header_offset, left: 0, behavior: 'smooth' });
@@ -41,7 +41,7 @@ jQuery(document).ready(function($){
     });
 
     //Smooth scroll to top
-    $('#to-top').click(function(){
+    $('#to-top').click(function () {
         window.scroll({ top: 0, left: 0, behavior: 'smooth' });
         return false;
     });
@@ -54,6 +54,6 @@ jQuery(document).ready(function($){
     });
 
     // Tree Menu
-    $(".tree").treemenu({delay:300});
+    $(".tree").treemenu({ delay: 300, openActive: true });
 
 });
